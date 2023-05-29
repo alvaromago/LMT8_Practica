12242a55e674a9660f53ce6f1a889023164f30a9
alvaromago <aalvaromago@gmail.com>
2023-05-29 10:19:39 +0200

    id's ejercicios

--- js/ej17.js
+++ js/ej17.js
@@ -1,6 +1,5 @@
 $num = prompt("Dame un número: ");
-document.write("<h3>Ejercicio 17</h3>");
 
-for (i = 0; i <= 10; i++){
-    document.write(`${$num}x` + i + "=" + i * $num + "<br>");
+for (i = 0; i <= 10; i++) {
+	document.write(`${$num}x` + i + "=" + i * $num + "<br>");
 }
