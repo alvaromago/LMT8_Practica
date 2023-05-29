c5b3019bc1d3a7bcc52c30c058830415e6e6d396
alvaromago <aalvaromago@gmail.com>
2023-05-29 09:31:49 +0200

    JQuery

--- js/ej20.js
+++ js/ej20.js
@@ -1,13 +1,12 @@
-document.write("<h3>Ejercicio 20</h3>");
 document.write("<p id='frase1'>Haz click en esta frase</p>");
 document.write("<p id='frase2'>Haz doble click en esta frase</p>");
 
-document.getElementById("frase1").onclick = function(event) {
-    alert("Has hecho click");
-    event.preventDefault();
-}
+document.getElementById("frase1").onclick = function (event) {
+	alert("Has hecho click");
+	event.preventDefault();
+};
 
-document.getElementById("frase2").ondblclick = function(event) {
-    alert("Has hecho doble click");
-    event.preventDefault();
-}
+document.getElementById("frase2").ondblclick = function (event) {
+	alert("Has hecho doble click");
+	event.preventDefault();
+};
---
+++ js/index.js
@@ -0,0 +1,3 @@
+$(function () {
+	// Código jQuery
+});
